ff3aad258b3facfbffc45df2d43eddd22ef140d1
minhngocln <minhngoc.ln@gmail.com>
2015-10-27 14:21:28 -0700

    added projects

--- about_me.js.jsx
+++ about_me.js.jsx
@@ -10,15 +10,16 @@ window.AboutMe = React.createClass({
           <img className="profile-photo" src="./me.png"/>
           I started out as a philosophy major with interests in
           analytic philosophy and formal logic, which eventually led me to learn
-          more about computers and computability.  I wrote my first "code"
-          senior year of college :  they were instructions for a Turing Machine.
+          more about computers and computability.  I wrote my first piece of code
+          senior year of college.  It was instructions for a Turing Machine.
            Though my introduction to the field was rather abstract and
           removed from what the majority of computer programmers actually do,
           I knew that I enjoyed the type of thinking that comes with being
           a programmer.  So, I started teaching myself how to code,
           and enrolled in App Academy, a bootcamp for web development.  So far,
           I've built a number of websites, mini games, and web widgets, using
-          Ruby on Rails, JavaScript, SQL, jQuery, React/Flux.
+          Ruby on Rails, JavaScript, SQL, jQuery, React/Flux. <a href="./Resume-Minh.pdf">Here's</a> a writeup
+          of my (professional) life so far.
         </div>
         <div value="ShowAboutMe" className="down button" onClick={this.props.handleClick}>{button}</div>
       </div>
--- projects.js.jsx
+++ projects.js.jsx
@@ -7,7 +7,30 @@ window.Projects = React.createClass({
     return (
       <div className={klass}>
         <div className="project-thumbs-container">
-
+          <a className="project-thumb-wrapper" href="https://github.com/anotherminh/Rent-a-Bench">
+            <img className="project-thumb" src="./images/benchIcon.png"></img>
+            Seat-Finder
+          </a>
+          <a className="project-thumb-wrapper" href="https://github.com/anotherminh/minesweeper">
+            <img className="project-thumb" src="./images/minesweeper.png"></img>
+            Minesweeper
+          </a>
+          <a className="project-thumb-wrapper" href="https://github.com/anotherminh/ruby_chess_game">
+            <img className="project-thumb" src="./images/chess.png"></img>
+            Chess
+          </a>
+          <a className="project-thumb-wrapper" href="https://github.com/anotherminh/mini-keyboard">
+            <img className="project-thumb" src="./images/keyboard.png"></img>
+            Mini Keyboard
+          </a>
+          <a className="project-thumb-wrapper" href="https://github.com/anotherminh/RailsLite">
+            <img className="project-thumb" src="./images/rails.png"></img>
+            Minhi Rails
+          </a>
+          <a className="project-thumb-wrapper" href="https://github.com/anotherminh/FlyingSheep">
+            <img className="project-thumb" src="./images/flying-sheep.png"></img>
+            FlyingSheep
+          </a>
         </div>
         <div value="ShowProjects" className="right button" onClick={this.props.handleClick}>{button}</div>
       </div>
