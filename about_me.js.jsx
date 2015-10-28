8680e326820c603bda659ea15ae0d7ce2893676a
minhngocln <minhngoc.ln@gmail.com>
2015-10-28 13:53:22 -0700

    fixed bio

--- about_me.js.jsx
+++ about_me.js.jsx
@@ -10,14 +10,14 @@ window.AboutMe = React.createClass({
           <img className="profile-photo" src="./me.png"/>
           I started out as a philosophy major with interests in
           analytic philosophy and formal logic, which eventually led me to learn
-          more about computers and computability.  I wrote my first piece of code
+          more about computability.  I wrote my first piece of code
           senior year of college.  It was instructions for a Turing Machine.
            Though my introduction to the field was rather abstract and
           removed from what the majority of computer programmers actually do,
           I knew that I enjoyed the type of thinking that comes with being
           a programmer.  So, I started teaching myself how to code,
           and enrolled in App Academy, a bootcamp for web development.  So far,
-          I've built a number of websites, mini games, and web widgets, using
+          I've built a number of websites, mini games, and web widgets using
           Ruby on Rails, JavaScript, SQL, jQuery, React/Flux. <a href="./Resume-Minh.pdf">Here's</a> a writeup
           of my (professional) life so far.
         </div>
